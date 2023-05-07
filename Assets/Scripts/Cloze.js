4a4887c52805de5e03c56c226e317f1fc94a973f
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-05-07 16:36:03 +0800

    updatee `Cloze`
    1. `Cloze`: Add various mode choices for "@auto_spell"

--- Assets/Scripts/Cloze.js
+++ Assets/Scripts/Cloze.js
@@ -9,6 +9,14 @@
             V0.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;〔" + auto_phrase(V0.dataset.cloze.replace(/<.+?>/g, "")) + "〕&nbsp;&nbsp;&nbsp;&nbsp;"
         } else if (hint === "@auto_spell") {
             V0.innerHTML = "<span style='display:inline-block; text-decoration: none;'>〔" + auto_spell(V0.dataset.cloze.replace(/<.+?>/g, "")) + "〕</span>"
+        } else if (hint === "@auto_spell_custom") {
+            V0.innerHTML = "<span style='display:inline-block; text-decoration: none;'>〔" + auto_spell(V0.dataset.cloze.replace(/<.+?>/g, ""), ":") + "〕</span>"
+        } else if (hint === "@auto_spell_one") {
+            V0.innerHTML = "<span style='display:inline-block; text-decoration: none;'>〔" + auto_spell(V0.dataset.cloze.replace(/<.+?>/g, ""), "#") + "〕</span>"
+        } else if (hint === "@auto_spell_all") {
+            V0.innerHTML = "<span style='display:inline-block; text-decoration: none;'>〔" + auto_spell(V0.dataset.cloze.replace(/<.+?>/g, ""), "-") + "〕</span>"
+        } else if (hint === "@auto_spell_wide") {
+            V0.innerHTML = "<span style='display:inline-block; text-decoration: none;'>〔" + auto_spell(V0.dataset.cloze.replace(/<.+?>/g, ""), "!") + "〕</span>"
         } else {
             V0.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;〔" + hint + "〕&nbsp;&nbsp;&nbsp;&nbsp;"
         }
@@ -25,19 +33,15 @@ function auto_phrase(phrase) {
     return words[0].toLowerCase()
 }
 
-function auto_spell(word) {
+function auto_spell(word, instruction = null) {
     let hiddenWords = ["?", "in", "of", "from", "at", "on", "through", "via", "before", "after", "until", "to", "down", "up", "off", "away", "into", "onto", "upon", "out", "with", "without", "within", "by", "for", "since", "toward", "towards", "forward", "forwards", "above", "below", "beyond", "again", "about", "under", "beneath", "during", "around", "than", "over", "beside", "behind", "as", "and", "or", "though", "but", "not", "nor", "either", "neither", "yet", "both", "so", "such", "that", "which", "when", "how", "what", "a", "an", "the", "this", "that", "if", "no", "most", "one", "one's", "oneself", "do", "does", "doing", "did", "done", "be", "is", "are", "was", "were", "been", "many", "much", "can", "can't", "sb's", "back", "there", "there's", "it", "it's", "other", "else", "among", "against", "things", "ever", "something", "everything", "anything", "i", "me", "my", "we", "us", "our", "you", "your", "he", "him", "his", "she", "her", "they", "them", "their", "whether", "whose", "very", "other", "others", "another", "upside", "should", "only", "ahead", "throughout", "once", "all", "b", "still", "what's", "aside", "some", "each"]
     let preservedWords = ["sth", "sb", "sp", "br"]
     let underline = "__";
     let partword = "";
-    let instruction = word[0];
     let result = ""
 
-    if ([":", "-", "!", "#"].includes(instruction)) {
-        word = word.slice(1)
-    } else {
+    if (![":", "-", "!", "#"].includes(instruction))
         instruction = null
-    }
 
     if (instruction === "#") {
         return underline.repeat(2);
