8963e9cadf5bc48203c8611975bde407fea9200b
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-09-03 19:51:55 +0800

    Update KaTeX macros & hidden words in English template

--- Assets/Scripts/Markdown-KaTeX.js
+++ Assets/Scripts/Markdown-KaTeX.js
@@ -81,60 +81,67 @@ function renderMath(ID) {
         ],
         trust: true,
         macros: {
-            //1 常数
-            "\\e": "\\mathrm{e}",//自然对数
-            "\\i": "\\mathrm{i}",//虚数单位
-            //2 代表符
-            "\\Q": "\\mathbb{Q}",//有理数集合
-            "\\C": "\\Complex",//复数集合
-            "\\empty": "\\varnothing",//空集//替换//原\empty为\emptyset
-            "\\circle": "\\odot",//圆
-            "\\circled": "\\textcircled{\\footnotesize\\text{#1}}",//圆圈
-            //3 运算符
-            "\\d": "\\mathop{}\\!\\mathrm{d}",//微分符号
-            "\\pd": "\\mathop{}\\!\\partial",//偏微分符号
-            "\\as": "\\bigg\\vert",//代入符号
-            "\\c": "\\operatorname{C}",//组合符号
-            "\\r": "\\operatorname{r}",//秩
-            "\\tr": "\\operatorname{tr}",//迹
-            "\\Re": "\\operatorname{Re}\\left(#1\\right)",//实数部分//替换//原\Re为\real
-            "\\Im": "\\operatorname{Im}\\left(#1\\right)",//虚数部分//替换//原\Im为\image
-            "\\le": "\\leqslant",//小于等于//替换//原\le为\leq
-            "\\ge": "\\geqslant",//大于等于//替换//原\ge为\geq
-            "\\nle": "\\nleqslant",//不小于等于
-            "\\nge": "\\ngeqslant",//不大于等于
-            "\\nl": "\\nless",//不小于
-            "\\ng": "\\ngtr",//不大于
-            //4 关系符
-            "\\par": "\\mathrel{/\\kern-5mu/}",//平行
-            "\\npar": "\\mathrel{/\\kern-13mu\\smallsetminus\\kern-13mu/}",//不平行
-            "\\nimplies": "\\mathrel{\\kern13mu\\not\\kern-13mu\\implies}",//无法推出
-            "\\nimpliedby": "\\mathrel{\\kern13mu\\not\\kern-13mu\\impliedby}",//无法被推出
-            "\\niff": "\\mathrel{\\kern13mu\\not\\kern-13mu\\iff}",//不等价
-            // 若不可用则使用下列宏
-            // "\\par": "/\\kern-5mu/",//平行
-            // "\\npar": "/\\kern-13mu\\smallsetminus\\kern-13mu/",//不平行
-            // "\\nimplies": "\\kern13mu\\not\\kern-13mu\\implies",//无法推出
-            // "\\nimpliedby": "\\kern13mu\\not\\kern-13mu\\impliedby",//无法被推出
-            // "\\niff": "\\kern13mu\\not\\kern-13mu\\iff",//不等价
-            //5 函数
-            "\\arccot": "\\operatorname{arccot}",//反余切函数
-            "\\arsinh": "\\operatorname{arsinh}",//反双曲正弦函数
-            "\\arcosh": "\\operatorname{arcosh}",//反双曲余弦函数
-            "\\artanh": "\\operatorname{artanh}",//反双曲正切函数
-            "\\arcoth": "\\operatorname{arcoth}",//反双曲余切函数
-            //6 特殊
-            "\\ssd": "{\\mathrm{\\degree\\kern-0.2em C}}",//摄氏度
-            "\\hsd": "{\\mathrm{\\degree\\kern-0.2em F}}",//华氏度
-            //7 旧项（如有依赖则取消注释）
-            // "\\env": "\\begin{#1}#2\\end{#1}",//环境
-            // "\\envo": "\\begin{#1}{#2}#3\\end{#1}",//环境+选项
-            // "\\pe": "\\kern-0.023em\\boxed{\\uparrow\\downarrow}\\kern-0.023em",//电子对
-            // "\\npe": "\\kern-0.023em\\boxed{\\uparrow\\uparrow}\\kern-0.023em",//错误电子对
-            // "\\nnpe": "\\kern-0.023em\\boxed{\\downarrow\\downarrow}\\kern-0.023em",//错误电子对
-            // "\\se": "\\kern-0.023em\\boxed{\\kern0.25em\\uparrow\\kern0.25em}\\kern-0.023em",//单电子
-            // "\\nse": "\\kern-0.023em\\boxed{\\kern0.25em\\downarrow\\kern0.25em}\\kern-0.023em",//单电子
-            // "\\oe": "\\kern-0.023em\\boxed{\\kern0.25em\\phantom\\uparrow\\kern0.25em}\\kern-0.023em",//空电子
+      //1 常数
+      "\\e": "\\mathrm{e}",//自然对数
+      "\\i": "\\mathrm{i}",//虚数单位
+      //2 代表符
+      "\\Q": "\\mathbb{Q}",//有理数集合
+      "\\C": "\\Complex",//复数集合
+      "\\empty": "\\varnothing",//空集//替换//原\empty为\emptyset
+      "\\circle": "\\odot",//圆
+      "\\circled": "\\textcircled{\\footnotesize\\text{#1}}",//圆圈
+      "\\Forall": "\\operatornamewithlimits{\\Large\\forall}_{#1}",//大全称量词
+      "\\Exists": "\\operatornamewithlimits{\\Large\\exists}_{#1}",//大存在量词
+      //3 运算符
+      "\\d": "\\mathop{}\\!\\mathrm{d}",//微分符号
+      "\\pd": "\\mathop{}\\!\\partial",//偏微分符号
+      "\\as": "\\bigg\\vert",//代入符号
+      "\\combination": "\\operatorname{C}",//组合符号
+      "\\rank": "\\operatorname{r}",//秩
+      "\\trace": "\\operatorname{tr}",//迹
+      "\\grad": "\\boldsymbol{\\nabla}",//梯度
+      "\\span": "\\operatorname{span}",//向量空间
+      "\\dim": "\\operatorname{dim}",//维数
+      "\\real": "\\mathord{\\char\"211c}",//原\real
+      "\\Re": "\\operatorname{Re}",//实数部分//替换//原\Re为\real
+      "\\image": "\\mathord{\\char\"2111}",//原\image
+      "\\Im": "\\operatorname{Im}",//虚数部分//替换//原\Im为\image
+      "\\le": "\\leqslant",//小于等于//替换//原\le为\leq
+      "\\ge": "\\geqslant",//大于等于//替换//原\ge为\geq
+      "\\nle": "\\nleqslant",//不小于等于
+      "\\nge": "\\ngeqslant",//不大于等于
+      "\\nl": "\\nless",//不小于
+      "\\ng": "\\ngtr",//不大于
+      //4 关系符
+      // "\\par": "\\mathrel{/\\kern-5mu/}",//平行
+      // "\\npar": "\\mathrel{/\\kern-13mu\\smallsetminus\\kern-13mu/}",//不平行
+      // "\\nimplies": "\\mathrel{\\kern13mu\\not\\kern-13mu\\implies}",//无法推出
+      // "\\nimpliedby": "\\mathrel{\\kern13mu\\not\\kern-13mu\\impliedby}",//无法被推出
+      // "\\niff": "\\mathrel{\\kern13mu\\not\\kern-13mu\\iff}",//不等价
+      // 若不可用则使用下列宏
+      "\\par": "/\\kern-5mu/",//平行
+      "\\npar": "/\\kern-13mu\\smallsetminus\\kern-13mu/",//不平行
+      "\\nimplies": "\\kern13mu\\not\\kern-13mu\\implies",//无法推出
+      "\\nimpliedby": "\\kern13mu\\not\\kern-13mu\\impliedby",//无法被推出
+      "\\niff": "\\kern13mu\\not\\kern-13mu\\iff",//不等价
+      //5 函数
+      "\\arccot": "\\operatorname{arccot}",//反余切函数
+      "\\arsinh": "\\operatorname{arsinh}",//反双曲正弦函数
+      "\\arcosh": "\\operatorname{arcosh}",//反双曲余弦函数
+      "\\artanh": "\\operatorname{artanh}",//反双曲正切函数
+      "\\arcoth": "\\operatorname{arcoth}",//反双曲余切函数
+      //6 特殊
+      "\\ssd": "{\\mathrm{\\degree\\kern-0.2em C}}",//摄氏度
+      "\\hsd": "{\\mathrm{\\degree\\kern-0.2em F}}",//华氏度
+      //7 旧项（如有依赖则取消注释）
+      "\\env": "\\begin{#1}#2\\end{#1}",//环境
+      "\\envo": "\\begin{#1}{#2}#3\\end{#1}",//环境+选项
+      // "\\pe": "\\kern-0.023em\\boxed{\\uparrow\\downarrow}\\kern-0.023em",//电子对
+      // "\\npe": "\\kern-0.023em\\boxed{\\uparrow\\uparrow}\\kern-0.023em",//错误电子对
+      // "\\nnpe": "\\kern-0.023em\\boxed{\\downarrow\\downarrow}\\kern-0.023em",//错误电子对
+      // "\\se": "\\kern-0.023em\\boxed{\\kern0.25em\\uparrow\\kern0.25em}\\kern-0.023em",//单电子
+      // "\\nse": "\\kern-0.023em\\boxed{\\kern0.25em\\downarrow\\kern0.25em}\\kern-0.023em",//单电子
+      // "\\oe": "\\kern-0.023em\\boxed{\\kern0.25em\\phantom\\uparrow\\kern0.25em}\\kern-0.023em",//空电子
         },
         throwOnError: false
     });
